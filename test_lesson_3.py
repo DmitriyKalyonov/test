3ec0ae3be9c0f287ec2c3b1208a0b0ebabb237be
DmitriyKalyonov <dmkalyonov@gmail.com>
2023-05-01 19:22:30 +0300

    code review: optimizaition code - line 16

--- test_lesson_3.py
+++ test_lesson_3.py
@@ -9,11 +9,11 @@ def cubs_game():
 
   while total_attemps > 0:
     comp_attemp = random.randint(1, 6)
-    player_attemp = random.randint(1, 6)
-
     print(f'Бросаем кости...\nБросок...\nКУБ КОМПЬЮТЕРА: {comp_attemp}')
-    attemp = input('Нажмите ENTER для броска: ')
-    print(f'КУБ ИГРОКА: {comp_attemp}')
+
+    input('Нажмите ENTER для броска: ')
+    player_attemp = random.randint(1, 6)
+    print(f'КУБ ИГРОКА: {player_attemp}')
 
     if comp_attemp > player_attemp:
       print('Ты проиграл!\n')
@@ -26,13 +26,13 @@ def cubs_game():
       comp_point += 1
       player_point += 1
 
-    print(f'     ТЕКУЩИЙ СЧЁТ')
-    print('======================')
+    print(f'    ТЕКУЩИЙ СЧЁТ')
+    print('=====================')
     print(f'ИГРОК - {player_point} : {comp_point} - КОМП\n')
 
     total_attemps -= 1
-  print(f'ИТОГОВЫЙ СЧЕТ')
-  print('================')
+  print(f'    ИТОГОВЫЙ СЧЕТ')
+  print('=====================')
   print(f'ИГРОК - {player_point} : {comp_point} - КОМП\n')
 
 cubs_game()
